8fc394704d587c96d3893e91907c1f07f6c71c12
tchisama <tchisama41@gmail.com>
2024-03-26 19:34:22 +0000

    i can create new branch now

--- src/core/agents/funs/branch.js
+++ src/core/agents/funs/branch.js
@@ -2,6 +2,7 @@ import * as p from "@clack/prompts";
 import color from "picocolors";
 import { getStatus, getStatusFiles } from "./status.js";
 import simpleGit from "simple-git";
+import { getRepositoryName, getCurrentBranch } from "./repo.js";
 
 async function listLocalBranches() {
   const git = simpleGit();
@@ -20,6 +21,7 @@ async function listLocalBranches() {
 export const branch = async () => {
   const git = simpleGit();
   const branchs = await listLocalBranches();
+  const currentBranch = await getCurrentBranch();
 
   return new Promise(async (resolve, reject) => {
     try {
@@ -29,17 +31,30 @@ export const branch = async () => {
           p.select({
             message: "Select files",
             initialValues: [],
-            options: branchs.map((b) => {
+            options: [...branchs, " \uf067 | craet branch "].map((b) => {
               return {
                 value: b,
-                label: b,
+                label: b==currentBranch ? color.blue(b + " *"):b,
               };
             }),
           }),
       });
-      git.checkout(selectedBranch.branch);
+      if (selectedBranch.branch !== " \uf067 | craet branch ") {
+        await git.checkout(selectedBranch.branch);
+      } else {
+        const newBranch = await p.group({
+          name: () =>
+            p.text({
+              message: "branch name",
+              validate: (value) => {
+                if (!value) return "cant be empy!";
+              },
+            }),
+        });
+        await git.checkoutBranch(newBranch.name, currentBranch);
+      }
 
-      resolve(selectedFiles.files);
+      resolve(selectedBranch.branch);
     } catch (error) {
       reject(error);
     }
